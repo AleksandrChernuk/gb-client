f884073a8d4a8e7a3b32a8f99f0df56221b048ee
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2025-02-09 22:13:56 +0200

    add react-phone-number-input

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,20 @@
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
+import { NextResponse, NextRequest } from "next/server";
 
-export default createMiddleware(routing);
+const intlMiddleware = createMiddleware(routing);
 
+export default function middleware(req: NextRequest) {
+  if (req.method === "OPTIONS") {
+    return new NextResponse(null, { status: 204 }); // No Content
+  }
+  return intlMiddleware(req);
+}
 
-
- export const config = {
-   matcher: [
-     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-     "/(en|ru|uk)/:path*",
-     "/(api|trpc)(.*)",
-   ],
- };
+export const config = {
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(en|ru|uk)/:path*",
+    "/(api|trpc)(.*)",
+  ],
+};
