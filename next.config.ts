cb369bcc0198fa5aaf661ad3edd7519e1a853ba4
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2025-03-16 17:07:31 +0200

    change layouts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: false };
+const nextConfig = { reactStrictMode: false, nextScriptWorkers: true };
 
 export default withNextIntl(nextConfig);
--- src/components/pages/main/components/HerowImg.tsx
+++ src/components/pages/main/components/HerowImg.tsx
@@ -4,17 +4,8 @@ import descImg from '../images/main.webp';
 
 export default function HerowImg() {
   return (
-    <div className="relative flex items-center justify-center w-full h-auto overflow-hidden">
-      <Image
-        src={descImg}
-        priority={true}
-        alt="peaple wait bus"
-        placeholder="blur"
-        style={{
-          width: '100%',
-          height: 'auto',
-        }}
-      />
+    <div className="relative flex items-center justify-center w-full max-h-[343px] overflow-hidden">
+      <Image src={descImg} alt="peaple wait bus" />
 
       {/* <Image
         className="hidden tablet:block"
