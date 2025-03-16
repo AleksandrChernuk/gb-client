57004f47e8bc760b2aac2785fbc438f44fb61aef
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2025-03-16 17:49:45 +0200

    change layouts

--- src/components/pages/main/components/HerowImg.tsx
+++ src/components/pages/main/components/HerowImg.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image';
 // import mobileImg from '../images/herow_mobile.webp';
-import descImg from '../images/main.webp';
+import descImg from '../images/main_1.png';
 
 export default function HerowImg() {
   return (
-    <div className="relative flex items-center justify-center w-full max-h-[343px] overflow-hidden">
-      <Image src={descImg} alt="peaple wait bus" />
+    <div className="relative flex items-center justify-center w-full  h-auto overflow-hidden">
+      <Image src={descImg} alt="peaple wait bus" placeholder="blur" quality={100} />
 
       {/* <Image
         className="hidden tablet:block"
