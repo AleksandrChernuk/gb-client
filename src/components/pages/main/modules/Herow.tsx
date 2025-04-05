216fc323639efdc987bed2250968c14957726663
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2025-04-05 12:52:36 +0300

    update filter

--- src/components/pages/main/modules/Herow.tsx
+++ src/components/pages/main/modules/Herow.tsx
@@ -4,14 +4,35 @@ import { Container } from '@/components/shared/Container';
 export default function Herow() {
   return (
     <section>
+      {/* <Image
+        src={mob_full}
+        alt="People waiting for the bus"
+        placeholder="blur"
+        priority
+        width={392}
+        height={140}
+        sizes="100vw"
+        className="w-full tablet:hidden"
+      />
+
+      <Image
+        src={desc_full}
+        alt="People waiting for the bus"
+        placeholder="blur"
+        width={1440}
+        height={233}
+        sizes="100vw"
+        className="w-full hidden tablet:block"
+      /> */}
+
       <picture>
-        <source media="(max-width: 767px)" srcSet="/images/mob_full.avif" />
+        <source media="(max-width: 767px)" srcSet="/images/mob_full.avif  1x, /images/mob_full_2x.avif 2x" />
         <source media="(min-width: 768px)" srcSet="/images/desc_full.avif" />
         <img
           src="/images/desc_full.avif"
           alt="People waiting for the bus"
-          width="100%"
-          height="auto"
+          width={1440}
+          height={233}
           className="w-full"
           loading="eager"
         />
