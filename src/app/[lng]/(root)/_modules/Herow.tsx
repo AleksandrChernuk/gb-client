fe7382357be1631bd788cb3b914621c683fd1b0c
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2025-04-29 23:01:01 +0300

    new

--- src/app/[lng]/(root)/_modules/Herow.tsx
+++ src/app/[lng]/(root)/_modules/Herow.tsx
@@ -4,6 +4,7 @@ import { Container } from '@/components/shared/Container';
 import { getTranslations } from 'next-intl/server';
 import Image from 'next/image';
 import desc from '@/assets/images/desc_full_2x.avif';
+import mob from '@/assets/images/mob_full_2x.avif';
 
 export default async function Herow() {
   const t_img_alts = await getTranslations('img_alts');
@@ -24,11 +25,27 @@ export default async function Herow() {
       </picture> */}
       <figure>
         <Image
+          src={mob}
+          alt={t_img_alts('herow')}
+          placeholder="blur"
+          width={740}
+          height={233}
+          priority
+          sizes="100vw"
+          className="tablet:hidden"
+          style={{
+            width: '100%',
+            height: 'auto',
+          }}
+        />
+        <Image
           src={desc}
           alt={t_img_alts('herow')}
           placeholder="blur"
           width={1240}
           height={233}
+          priority
+          className="hidden tablet:block"
           sizes="100vw"
           style={{
             width: '100%',
