ea9a54b48525f936889d3fc28d1549f113f3a05d
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2025-05-03 18:47:00 +0300

    end

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,13 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import Error from 'next/error';
+// import Error from 'next/error';
 
 export default function GlobalNotFound() {
   return (
     <html lang="en">
       <body>
-        <Error statusCode={404}>
-          <Button>Gohome</Button>
-        </Error>
-        ;
+        <Button>Gohome</Button>
       </body>
     </html>
   );
