00f5fe5b7dc4f5ae6a10b977cb82231fe4eb52c0
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2025-05-04 12:32:03 +0300

    end

--- src/app/[lng]/(root)/_modules/HerowImages.tsx
+++ src/app/[lng]/(root)/_modules/HerowImages.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/alt-text */
 import { getImageProps } from 'next/image';
 import desc from '@/assets/images/desc_full_2x.avif';
-import mob from '@/assets/images/mob_full_2x.avif';
+import mob from '@/assets/images/mob_full.avif';
 
 export default function HerowImages() {
   const common = { alt: 'Art Direction Example', sizes: '100vw' };
@@ -22,6 +22,7 @@ export default function HerowImages() {
     ...common,
     width: 750,
     height: 233,
+    quality: 80,
     src: mob,
     loading: 'eager',
     decoding: 'sync',
