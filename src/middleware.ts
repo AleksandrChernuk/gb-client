dc762d1a3c01ec25d40c39359b56dbbe4afb76a6
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2025-05-04 11:14:28 +0300

    end

--- src/middleware.ts
+++ src/middleware.ts
@@ -24,5 +24,12 @@ export default createMiddleware(routing);
 // }
 
 export const config = {
-  matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)',
+  matcher: [
+    // Редирект с корня на дефолтную локаль
+    '/',
+    // Устанавливаем куку при заходе с локального префикса
+    '/(en|ru|uk)/:path*',
+    // Автодополнение недостающего префикса
+    '/((?!_next|_vercel|.*\\..*).*)',
+  ],
 };
