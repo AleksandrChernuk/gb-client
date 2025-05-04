7b04c1f370e6885564b777f936f3e22ef498791b
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2025-05-04 10:13:53 +0300

    end

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,12 +2,20 @@ import { defineRouting } from 'next-intl/routing';
 import { createNavigation } from 'next-intl/navigation';
 
 export const routing = defineRouting({
-  locales: ['en', 'uk', 'ru'],
+  locales: ['en', 'ru', 'uk'],
   defaultLocale: 'uk',
   localeCookie: {
     maxAge: 60 * 60 * 24 * 365,
     secure: true,
     sameSite: 'lax',
+  },
+  localePrefix: {
+    mode: 'always',
+    prefixes: {
+      en: '/en',
+      ru: '/ru',
+      uk: '/uk',
+    },
   },
 });
 
