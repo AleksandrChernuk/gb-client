f8482026cdb41cac4bbb16102a053804a82aadd0
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2025-05-04 12:45:07 +0300

    end

--- src/app/[lng]/(root)/_modules/Herow.tsx
+++ src/app/[lng]/(root)/_modules/Herow.tsx
@@ -1,29 +1,24 @@
 import MainSearch from '@/components/modules/main-search';
 import { Container } from '@/components/shared/Container';
 import { getTranslations } from 'next-intl/server';
-// import Image from 'next/image';
-// import desc from '@/assets/images/desc_full_2x.avif';
-// import mob from '@/assets/images/mob_full_2x.avif';
+import Image from 'next/image';
+import desc from '@/assets/images/desc_full_2x.avif';
+import mob from '@/assets/images/mob_full_2x.avif';
 import { MESSAGE_FILES } from '@/constans/message.file.constans';
-import HerowImages from './HerowImages';
 
 export default async function Herow() {
-  // const t_img_alts = await getTranslations(MESSAGE_FILES.IMG_ALTS);
+  const t_img_alts = await getTranslations(MESSAGE_FILES.IMG_ALTS);
   const t_main = await getTranslations(MESSAGE_FILES.MAIN_PAGE);
 
   return (
     <section className="relative">
-      {/* <Image
+      <Image
         src={mob}
         alt={t_img_alts('herow')}
         placeholder="blur"
         width={740}
         height={233}
-        className="tablet:hidden"
-        style={{
-          width: '100%',
-          height: 'auto',
-        }}
+        className="tablet:hidden w-dvw h-auto"
         priority
         loading="eager"
         decoding="sync"
@@ -35,17 +30,13 @@ export default async function Herow() {
         placeholder="blur"
         width={1240}
         height={233}
-        className="hidden tablet:block"
-        sizes="100vw"
-        style={{
-          width: '100%',
-          height: 'auto',
-        }}
+        className="hidden tablet:block w-dvw h-auto"
         priority
         loading="eager"
         decoding="sync"
-      /> */}
-      <HerowImages />
+        quality={80}
+      />
+      {/* <HerowImages /> */}
 
       <Container size="l" className="-mt-10">
         <div>
