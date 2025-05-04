2be2317531647757d626b2ad5b0959e2278e81dd
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2025-05-04 11:48:30 +0300

    end

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,17 +1,24 @@
-import type { MetadataRoute } from 'next';
+import { MetadataRoute } from 'next';
+import { Locale } from 'next-intl';
+import { host } from '@/config';
+import { getPathname, routing } from '@/i18n/routing';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: 'https://greenbus.com.ua',
-      lastModified: new Date(),
-      alternates: {
-        languages: {
-          en: 'https://greenbus.com.ua/en',
-          uk: 'https://greenbus.com.ua/uk',
-          ru: 'https://greenbus.com.ua/ru',
-        },
-      },
+  return [...getEntries('/'), ...getEntries('/pathnames')];
+}
+
+type Href = Parameters<typeof getPathname>[0]['href'];
+
+function getEntries(href: Href) {
+  return routing.locales.map((locale) => ({
+    url: getUrl(href, locale),
+    alternates: {
+      languages: Object.fromEntries(routing.locales.map((cur) => [cur, getUrl(href, cur)])),
     },
-  ];
+  }));
+}
+
+function getUrl(href: Href, locale: Locale) {
+  const pathname = getPathname({ locale, href });
+  return host + pathname;
 }
