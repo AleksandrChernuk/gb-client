8ddd0505f06df7b3d4c692b47dae36967cf95bd0
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2025-07-16 10:57:32 +0300

    update

--- src/store/useTicketsDetails/index.ts
+++ src/store/useTicketsDetails/index.ts
@@ -5,7 +5,6 @@ import { IRouteDetailsResponse } from '@/types/route.details.interface';
 import { getRouteDetails } from '@/actions/route.actions';
 import { IGetRouteDetailsBody, IRouteResponse } from '@/types/route.types';
 import { TicketsDetailsStore } from './types';
-import { cleanObject } from '@/utils/cleanObject';
 
 export const useTicketsDetails = create<TicketsDetailsStore>()(
   devtools(
@@ -60,31 +59,34 @@ export const useTicketsDetails = create<TicketsDetailsStore>()(
 
         try {
           const rawData = {
-            routeId: route.identificators.route_id,
+            ...(!!route.identificators.route_id ? { routeId: `${route.identificators.route_id}` } : {}),
             intervalId: route.identificators.intervalId || '',
-            bus_id: route.provider_name === 'INFOBUS' ? undefined : route.identificators.bus_id,
-            // ...(route.provider_name !== 'INFOBUS' && route.identificators.bus_id
-            //   ? { bus_id: route.identificators.bus_id }
-            //   : {}),
+            ...(!!route.identificators.bus_id ? { bus_id: route.identificators.bus_id } : {}),
             fromCityId,
             toCityId,
-            fromStationId: route.departure.station_id,
-            toStationId: route.arrival.station_id,
+            fromStationId: `${route.departure.station_id}`,
+            toStationId: `${route.arrival.station_id}`,
             providerId: route.identificators.provider_id,
             travelDate,
             currency: 'UAH',
             locale,
             passengersCount: passCount,
-            metadata: route.identificators.metadata,
-            timetable_id: route.identificators.timetable_id,
-            bustype_id: route.identificators.bustype_id!,
-            has_plan: route.identificators.has_plan,
-            request_get_free_seats: route.identificators.request_get_free_seats,
-            request_get_discount: route.identificators.request_get_discount,
-            request_get_baggage: route.identificators.request_get_baggage,
+            ...(!!route.identificators.metadata ? { metadata: route.identificators.metadata } : {}),
+            ...(!!route.identificators.timetable_id ? { timetable_id: route.identificators.timetable_id } : {}),
+            ...(!!route.identificators.bustype_id ? { bustype_id: route.identificators.bustype_id } : {}),
+            ...(!!route.identificators.has_plan ? { has_plan: route.identificators.has_plan } : {}),
+            ...(!!route.identificators.request_get_free_seats
+              ? { request_get_free_seats: route.identificators.request_get_free_seats }
+              : {}),
+            ...(!!route.identificators.request_get_discount
+              ? { request_get_discount: route.identificators.request_get_discount }
+              : {}),
+            ...(!!route.identificators.request_get_baggage
+              ? { request_get_baggage: route.identificators.request_get_baggage }
+              : {}),
           };
 
-          res = await getRouteDetails(cleanObject(rawData) as IGetRouteDetailsBody);
+          res = await getRouteDetails(rawData as IGetRouteDetailsBody);
         } catch (error) {
           console.error('Ошибка при получении данных маршрута:', error);
           set((state) => ({
