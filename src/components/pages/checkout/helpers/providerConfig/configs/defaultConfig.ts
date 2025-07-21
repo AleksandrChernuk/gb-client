e596ebbf3baa0f365754bdbc35c89a87c485dd35
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2025-07-21 23:59:35 +0300

    change newOrderTypes

--- src/components/pages/checkout/helpers/providerConfig/configs/defaultConfig.ts
+++ src/components/pages/checkout/helpers/providerConfig/configs/defaultConfig.ts
@@ -4,7 +4,10 @@ import { FIELDS } from '../constans';
 import { bday, discount, first_name, last_name } from './config.list';
 
 const defaultConfig = (currentTicket: IRouteResponse | null): ProviderConfig => {
-  const hasDiscounts = !!currentTicket?.details?.discounts?.length && currentTicket.details.discounts[0].id !== '1210';
+  const hasDiscounts =
+    !!currentTicket?.details?.discounts?.length &&
+    currentTicket.details.discounts[0].id !== '1210' &&
+    currentTicket.details.discounts[0].id !== '1211';
 
   return {
     required: [FIELDS.first_name, FIELDS.last_name, ...(hasDiscounts ? ['discount', 'bday'] : [])],
