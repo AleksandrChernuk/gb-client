fae538fa123f90894b5f1d2940dd74ce98a676cd
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2025-09-29 21:46:52 +0300

    fsd

--- src/app/[lng]/layout.tsx
+++ src/app/[lng]/layout.tsx
@@ -49,35 +49,6 @@ export default async function MainLayout({
 
   return (
     <html lang={lng} dir={getTextDirection(lng as Locale)} suppressHydrationWarning>
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-
-        {/* Favicon и Apple Touch Icons */}
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png" />
-        <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png" />
-        <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png" />
-        <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png" />
-        <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png" />
-
-        {/* Preconnect для ускорения загрузки */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-
-        {/* PWA мета-теги */}
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="GreenBus" />
-
-        {/* Мета-теги для безопасности */}
-        <meta name="referrer" content="strict-origin-when-cross-origin" />
-      </head>
-
       <body className={`${notoSans.className} ${notoSans.variable} antialiased`} suppressHydrationWarning>
         <NextIntlClientProvider messages={messages}>
           <GTMNoScript />
