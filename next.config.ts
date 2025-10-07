d49cd64992c402c7501b5ac5d72bd770c16c8af9
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2025-10-07 20:13:39 +0300

    date time

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,18 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  optimizePackageImports: [
+    'react-icons',
+    '@radix-ui/react-accordion',
+    '@radix-ui/react-dialog',
+    '@radix-ui/react-dropdown-menu',
+    '@radix-ui/react-select',
+    '@radix-ui/react-tooltip',
+    'react-hook-form',
+    'zod',
+    'clsx',
+    'tailwind-merge',
+  ],
   reactStrictMode: false,
 };
 
