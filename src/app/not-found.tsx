114941867b7441ab9139c26647596f2af2e8bada
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2025-10-12 23:08:02 +0300

    date time

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,28 +1,38 @@
-import { Container } from '@/shared/ui/Container';
-import CustomCard from '@/shared/ui/CustomCard';
-import errorImg from '@/assets/images/something-happened-on-the-site.avif';
-import Image from 'next/image';
-import { Button } from '@/shared/ui/button';
-import { Link } from '@/shared/i18n/routing';
+'use client';
 
-export default function NotFound() {
+import { Button } from '@/shared/ui/button';
+import Link from 'next/link';
+
+export default function GlobalNotFound() {
   return (
-    <main className="grow flex flex-col items-center justify-center">
-      <section className="py-5">
-        <Container size="l" className="w-full">
-          <CustomCard className="shadow-xs flex flex-col items-center self-center gap-4 p-5 mx-auto text-center w-fit dark:bg-slate-700">
-            <div className="relative w-[313px] h-[313px] mx-auto overflow-hidden rounded-3xl">
-              <Image src={errorImg} draggable={false} placeholder="blur" alt="people waiting for bus" />
-            </div>
-            <h1 className="text-2xl laptop:text-[32px] font-bold leading-[28.8px] laptop:leading-[38.4px] text-slate-700 dark:text-slate-50">
-              Сторінку не знайдено
-            </h1>
-            <Button variant="secondary" size="primary" className="text-black">
-              <Link href="/">На головну</Link>
-            </Button>
-          </CustomCard>
-        </Container>
-      </section>
-    </main>
+    <html lang="en">
+      <body
+        style={{
+          minHeight: '100vh',
+          margin: 0,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: '#f9fafb',
+          fontFamily: 'sans-serif',
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: '#fff',
+            borderRadius: '12px',
+            boxShadow: '0 4px 10px rgba(0,0,0,0.1)',
+            padding: '40px 60px',
+            textAlign: 'center',
+          }}
+        >
+          <h1 style={{ fontSize: '64px', margin: 0, color: '#111827' }}>404</h1>
+          <p style={{ fontSize: '18px', color: '#6b7280', marginTop: '10px' }}>Page not found</p>
+          <Button asChild variant={'default'}>
+            <Link href={'/'}>Home</Link>
+          </Button>
+        </div>
+      </body>
+    </html>
   );
 }
