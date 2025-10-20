3820b5ad7ed3fea031216f699072466177d1aafa
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2025-10-20 18:58:37 +0300

    on pay

--- src/features/checkout-form/ui/SubmitButton.tsx
+++ src/features/checkout-form/ui/SubmitButton.tsx
@@ -8,16 +8,16 @@ import { memo, useMemo } from 'react';
 import { useFormContext } from 'react-hook-form';
 import { MESSAGE_FILES } from '@/shared/configs/message.file.constans';
 
-// import { Dialog, DialogContent } from '@/shared/ui/dialog';
-// import { DialogDescription, DialogTitle } from '@radix-ui/react-dialog';
-// import { useState } from 'react';
+import { Dialog, DialogContent } from '@/shared/ui/dialog';
+import { DialogDescription, DialogTitle } from '@radix-ui/react-dialog';
+import { useState } from 'react';
 
 const SubmitButton = memo(function SubmitButton() {
   const t = useTranslations(MESSAGE_FILES.CHECKOUT_PAGE);
   const loadingResult = useNewOrderResult((state) => state.loadingResult);
   const form = useFormContext();
 
-  // const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const isLoading = useMemo(
     () => loadingResult || form.formState.isSubmitting,
@@ -29,19 +29,19 @@ const SubmitButton = memo(function SubmitButton() {
       <Button
         variant="default"
         size="primary"
-        type="submit"
+        type="button"
         className="w-full"
-        // onClick={() => setOpen((p) => !p)}
+        onClick={() => setOpen((p) => !p)}
         disabled={isLoading}
       >
         {isLoading ? <LoaderCircle className="animate-spin" /> : t('confirm')}
       </Button>
-      {/* <Dialog open={open} onOpenChange={setOpen}>
+      <Dialog open={open} onOpenChange={setOpen}>
         <DialogContent className="text-center ">
           <DialogTitle className="font-bold">{t('service_unavailable_title')}</DialogTitle>
           <DialogDescription className="dark:text-slate-200">{t('service_unavailable_desc')}</DialogDescription>
         </DialogContent>
-      </Dialog> */}
+      </Dialog>
     </>
   );
 });
