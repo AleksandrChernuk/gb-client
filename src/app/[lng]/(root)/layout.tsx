644cfe84d55f425f942d9451e0d49f010655f6d5
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2025-12-08 22:32:01 +0200

    next 16

--- src/app/[lng]/(root)/layout.tsx
+++ src/app/[lng]/(root)/layout.tsx
@@ -1,3 +1,6 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 import { ReactNode } from 'react';
 import { Locale } from 'next-intl';
 import { setRequestLocale } from 'next-intl/server';
