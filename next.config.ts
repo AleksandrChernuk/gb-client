4d31e2729c6fd17b07eeb3810f472d91ebdce89e
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2026-01-06 22:57:14 +0200

    add search order

--- next.config.ts
+++ next.config.ts
@@ -25,11 +25,18 @@ const nextConfig: NextConfig = {
     ];
   },
 
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+
   images: {
-    formats: ['image/avif'],
+    formats: ['image/avif', 'image/webp'],
     minimumCacheTTL: 2678400,
     qualities: [25, 50, 75],
-
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     remotePatterns: [
       {
         protocol: 'https',
