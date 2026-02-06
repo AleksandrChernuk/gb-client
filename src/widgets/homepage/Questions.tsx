2715771861bfb46ffe79b93aaaa5f4ba78266513
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2026-02-06 11:02:32 +0200

    faq scroll

--- src/widgets/homepage/Questions.tsx
+++ src/widgets/homepage/Questions.tsx
@@ -35,7 +35,7 @@ export default async function Questions() {
         </ul>
         <div className="text-right">
           <Link
-            prefetch={false}
+            scroll={true}
             href={'/faq'}
             className="underline-offset-4 py-2 px-6 tablet:py-4 rounded-full tablet:text-base font-bold leading-6 tracking-normal min-w-[168px] min-h-[48px] max-h-[48px] tablet:max-h-[52px] text-green-600! hover:underline transition-all"
           >
