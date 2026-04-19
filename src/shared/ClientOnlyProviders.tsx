339ac4e9804aa6e23105b51c9c75de8a4f9adc62
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2026-04-19 23:45:08 +0300

    favicon

--- src/shared/ClientOnlyProviders.tsx
+++ src/shared/ClientOnlyProviders.tsx
@@ -2,7 +2,7 @@
 
 import dynamic from 'next/dynamic';
 
-import { GoogleTagManager } from '@next/third-parties/google';
+// import { GoogleTagManager } from '@next/third-parties/google';
 
 const Toaster = dynamic(() => import('sonner').then((m) => m.Toaster), { ssr: false });
 const LocationsInitializer = dynamic(() => import('@/entities/locations/LocationsInitializer'), { ssr: false });
@@ -15,7 +15,7 @@ export default function ClientOnlyProviders() {
       <LocationsInitializer />
       <Toaster richColors position="top-center" closeButton duration={4000} />
 
-      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID || 'GTM-MXK3BV2C'} />
+      {/* <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID || 'GTM-MXK3BV2C'} /> */}
     </>
   );
 }
